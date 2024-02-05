348f0de4c9a4700ede1fb41e14104a06b0a4773c
cisdeli <pedrohcisdeli@gmail.com>
2024-02-04 22:09:13 -0600

    Adapted Game struct to fit part2 and finished part2

--- src/day02.rs
+++ src/day02.rs
@@ -1,7 +1,7 @@
 /*
  * ROCK PAPER SCISSORS
  * Input: Encrypted strategy guide
- *  - First column is what the opponent is going to play.
+ *  - First column is what the first is going to play.
  *  - A: Rock
  *  - B: Paper
  *  - C: Scissors
@@ -9,6 +9,11 @@
  *  - X: Rock
  *  - Y: Paper
  *  - Z: scissors.
+ *  ** Part 2**
+ *  - Second column is the result of the match.
+ *  - X: lose
+ *  - Y: draw
+ *  - Z: win
  * Winner of the tournament is the player with highest score
  * The shape played affect your score:
  * - Rock: 1
@@ -24,74 +29,126 @@ enum Play {
     Paper,
     Scissors,
 }
+#[derive(Debug, PartialEq, Eq)]
+enum Result {
+    Lost,
+    Draw,
+    Win,
+}
 #[derive(Debug)]
 struct Game {
-    opponent: Play,
-    me: Play,
+    first: Play,
+    second: Play,
+    second_pt2: Result,
 }
 
 impl Game {
-    fn new(opp_play: char, my_play: char) -> Option<Self> {
-        let opponent = match opp_play {
+    fn new(first_col: char, second_col: char, second_col_pt2: char) -> Option<Self> {
+        let first = match first_col {
             'A' => Some(Play::Rock),
             'B' => Some(Play::Paper),
             'C' => Some(Play::Scissors),
             _ => None,
         };
-        let me = match my_play {
+        let second = match second_col {
             'X' => Some(Play::Rock),
             'Y' => Some(Play::Paper),
             'Z' => Some(Play::Scissors),
             _ => None,
         };
-        opponent.and_then(|a| me.map(|b| Game { opponent: a, me: b }))
+        let second_pt2 = match second_col_pt2 {
+            'X' => Some(Result::Lost),
+            'Y' => Some(Result::Draw),
+            'Z' => Some(Result::Win),
+            _ => None,
+        };
+        first.and_then(|a| {
+            second.and_then(|b| {
+                second_pt2.map(|c| Game {
+                    first: a,
+                    second: b,
+                    second_pt2: c,
+                })
+            })
+        })
     }
 
-    fn game_logic(&self) -> &str {
-        match (&self.opponent, &self.me) {
+    fn game_logic(&self) -> Result {
+        match (&self.first, &self.second) {
             (Play::Rock, Play::Scissors)
             | (Play::Paper, Play::Rock)
-            | (Play::Scissors, Play::Paper) => "opp",
+            | (Play::Scissors, Play::Paper) => Result::Lost,
 
             (Play::Rock, Play::Paper)
             | (Play::Paper, Play::Scissors)
-            | (Play::Scissors, Play::Rock) => "me",
+            | (Play::Scissors, Play::Rock) => Result::Win,
 
-            _ => "draw",
+            _ => Result::Draw,
+        }
+    }
+    fn game_logic_2(&self) -> Play {
+        match (&self.first, &self.second_pt2) {
+            (Play::Rock, Result::Lost) => Play::Scissors,
+            (Play::Scissors, Result::Lost) => Play::Paper,
+            (Play::Paper, Result::Lost) => Play::Rock,
+
+            (Play::Rock, Result::Win) => Play::Paper,
+            (Play::Scissors, Result::Win) => Play::Rock,
+            (Play::Paper, Result::Win) => Play::Scissors,
+
+            (Play::Rock, Result::Draw) => Play::Rock,
+            (Play::Scissors, Result::Draw) => Play::Scissors,
+            (Play::Paper, Result::Draw) => Play::Paper,
         }
     }
 
-    fn calculate_score(&self) -> u32 {
+    fn calculate_score(&self) -> (u32, u32) {
         let result = self.game_logic();
+        let result2 = self.game_logic_2();
 
-        let mut score = match self.me {
+        let mut score = match self.second {
+            Play::Rock => 1,
+            Play::Paper => 2,
+            Play::Scissors => 3,
+        };
+        let mut score2 = match result2 {
             Play::Rock => 1,
             Play::Paper => 2,
             Play::Scissors => 3,
         };
 
         score += match result {
-            "opp" => 0,
-            "me" => 6,
-            "draw" => 3,
-            _ => 0, // Handle unexpected results (you might want to log an error or panic here)
+            Result::Lost => 0,
+            Result::Draw => 3,
+            Result::Win => 6,
         };
-        return score;
-    } 
+        score2 += match self.second_pt2 {
+            Result::Lost => 0,
+            Result::Draw => 3,
+            Result::Win => 6,
+        };
+
+        return (score, score2);
+    }
 }
 
 pub fn solution(input: String) {
-    let mut score = 0;
+    let mut score_pt1 = 0;
+    let mut score_pt2 = 0;
     for line in input.lines() {
         let plays: Vec<&str> = line.split_whitespace().collect();
-        let (opp_play, my_play) = (
-            plays[0].chars().next().unwrap_or(' '),  // Default to space if None
-            plays[1].chars().next().unwrap_or(' '),  // Default to space if None
+        let (first_col, second_col) = (
+            plays[0].chars().next().unwrap_or(' '), // Default to space if None
+            plays[1].chars().next().unwrap_or(' '), // Default to space if None
         );
-        let game = Game::new(opp_play, my_play).unwrap_or_else(|| {
+        let game = Game::new(first_col, second_col, second_col).unwrap_or_else(|| {
             panic!("Invalid plays provided");
         });
-        score += game.calculate_score();
+
+        let (aux_score_pt1, aux_score_pt2) = game.calculate_score();
+        score_pt1 += aux_score_pt1;
+        score_pt2 += aux_score_pt2;
     }
-    println!("Score: {}", score);
+    println!("Score part 1: {}", score_pt1);
+    println!("Score part 2: {}", score_pt2);
 }
