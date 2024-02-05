4a3089479c934594530edcba0fed6ef7aaf1cb0f
cisdeli <pedrohcisdeli@gmail.com>
2024-02-05 17:49:37 -0600

    Finished day03 part2

--- src/day03.rs
+++ src/day03.rs
@@ -7,8 +7,10 @@
  * - Priorities:
  *  - a throught z has priorities 1-26
  *  - A-Z = 27-52
- * Output: find the sum of the priority of the item type that appers
+ * Output: find the sum of the priority of the item type that appears
  * in both compartments of each rucksack.
+ * Output for part 2: find the sum of the priorities of the item type that appears
+ * on the group of 3 elves (find the letter that appers on groups of 3 lines)
  */
 
 use std::collections::HashSet;
@@ -21,7 +23,7 @@ fn char_to_int(c: char) -> Option<u32> {
     }
 }
 
-fn sum_priorities(rucksack: String) -> u32 {
+fn sum_priorities_part_one(rucksack: String) -> u32 {
     if rucksack.len() % 2 != 0 {
         panic!("Can't split rucksack into two compartments of the same size");
     }
@@ -40,10 +42,42 @@ fn sum_priorities(rucksack: String) -> u32 {
     return sum;
 }
 
+fn sum_priorities_part_two(group_of_rucksacks: &Vec<String>) -> u32 {
+    let mut sum = 0;
+    // By converting the strings to hashsets with exclude any dupes
+    // Then we can use the function intersect() to check the char in common
+    let set1: HashSet<_> = group_of_rucksacks[0].chars().collect();
+    let set2: HashSet<_> = group_of_rucksacks[1].chars().collect();
+    let set3: HashSet<_> = group_of_rucksacks[2].chars().collect();
+    // Really cool way of intersecting multiple hashsets
+    // see: https://www.reddit.com/r/rust/comments/5v35l6/intersection_of_more_than_two_sets/
+    // for more information
+    let chars_in_common = set1.iter().filter(|k| set2.contains(k)).filter(|k| set3.contains(k));
+    for c in chars_in_common {
+        sum += char_to_int(*c).unwrap_or_else(|| {
+                println!("Invalid character: {}", c);
+                0
+            });
+    }
+    return sum;
+}
+
 pub fn solution(input: String) {
     let mut sum = 0;
+    let mut sum_pt2 = 0;
+    let mut i = 0;
+    let mut group_of_3: Vec<String> = vec![];
     for line in input.lines() {
-        sum += sum_priorities(line.to_string());
+        sum += sum_priorities_part_one(line.to_string());
+
+        group_of_3.push(line.to_string());
+        i += 1;
+        if i == 3 {
+            sum_pt2 += sum_priorities_part_two(&group_of_3);
+            group_of_3.clear();
+            i = 0
+        }
     }
     println!("Sum for part 1: {}", sum);
+    println!("Sum for part 2: {}", sum_pt2);
 }
