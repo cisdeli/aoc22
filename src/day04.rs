1326430f405967765c921d9bd1e57eacaaaa39d1
cisdeli <pedrohcisdeli@gmail.com>
2024-02-07 16:25:39 -0600

    Finished part2 of day 04

--- src/day04.rs
+++ src/day04.rs
@@ -2,6 +2,7 @@
  * CAMP CLEANUP
  * Input: Section assignments for each pair of elves
  * Output: how many assignments pairs does one range fully contain the other
+ * Output 2: how many assignments pairs does one range overlaps the other
  */
 
 fn parse_pair(pair: &str) -> (u32, u32) {
@@ -11,28 +12,43 @@ fn parse_pair(pair: &str) -> (u32, u32) {
     return (start, end);
 }
 
-fn check_pair(pair_one: &str, pair_two: &str) -> bool {
+fn check_pairs(pair_one: &str, pair_two: &str) -> (bool, bool) {
     let (start1, end1) = parse_pair(pair_one);
     let (start2, end2) = parse_pair(pair_two);
 
+    // Checking if pair contains the other
     let case1 = start1 <= start2 && end1 >= end2;
     let case2 = start2 <= start1 && end2 >= end1;
-    return case1 || case2;
+    let contains = case1 || case2;
+
+    // Checking if overlaps
+    let overlap1 = start1 <= start2 && end1 >= start2 && end1 <= end2;
+    let overlap2 = start2 <= start1 && end2 >= start1 && end2 <= end1;
+    let overlap3 = start1 >= start2 && end1 <= end2;
+    let overlap4 = start2 >= start1 && end2 <= end1;
+    let overlaps = overlap1 || overlap2 || overlap3 || overlap4;
+    
+    return (contains, overlaps);
 }
 
 pub fn solution(input: String) {
-    // let input = "2-4,6-8\n2-3,4-5\n5-7,7-9\n2-8,3-7\n6-6,4-6\n2-6,4-8";
-    let mut count = 0;
+    let mut contains_count = 0;
+    let mut overlaps_count = 0;
     for line in input.lines() {
         let mut pairs = line.split(',');
         if let (Some(pair_one), Some(pair_two)) = (pairs.next(), pairs.next()) {
-            if check_pair(pair_one, pair_two) {
-                count += 1;
+            let (contains, overlaps) = check_pairs(pair_one, pair_two);
+            if contains {
+                contains_count += 1;
+            }
+            if overlaps {
+                overlaps_count += 1;
             }
         } else {
             println!("Invalid pair format: {}", line);
         }
     }
     
-    println!("Count: {}", count);
+    println!("Contains Count: {}", contains_count);
+    println!("Overlaps Count: {}", overlaps_count);
 }
