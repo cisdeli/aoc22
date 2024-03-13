e6d802369e79969ccabe23495634ea942c21103a
cisdeli <pedrohcisdeli@gmail.com>
2024-03-12 22:04:56 -0500

    All data structures including crates are now correct; day 05 pt 1

--- src/day05.rs
+++ src/day05.rs
@@ -1,7 +1,5 @@
 /*
  * SUPPLY STACKS
- * Input:
- * Output:
  */
 
 use std::collections::HashMap;
@@ -9,26 +7,42 @@ use std::fs::File;
 use std::io::BufReader;
 use std::io::Read;
 
-fn parse_input(input: String) -> (HashMap<char, i32>, Vec<(char, i32, i32)>) {
-    let mut crates: HashMap<char, i32> = HashMap::new();
+fn fill_empty_crates(lines: Vec<&str>, max_height: usize) -> String {
+    let mut crates_str = String::new();
+
+    for i in 0..max_height {
+        let line = lines[i];
+        let aux = &format!("{}{}\n", line.replace("    ", "[-]"), "");
+        crates_str.push_str(&aux.replace(" ", ""));
+    }
+
+    crates_str
+}
+
+fn parse_input(input: String) -> (HashMap<i32, Vec<char>>, Vec<(char, i32, i32)>) {
+    let mut crates: HashMap<i32, Vec<char>> = HashMap::new();
     let mut moves: Vec<(char, i32, i32)> = Vec::new();
     let lines: Vec<&str> = input.lines().collect();
 
-    let num_crates = lines
+    let max_height = lines
         .iter()
         .take_while(|line| line.trim().starts_with('['))
         .count();
 
-    // Parse the initial positions of the crates
-    for i in 0..num_crates {
-        let line = lines[i];
-        let position = i + 1;
-        for c in line.chars().filter(|c| c.is_alphabetic()) {
-            crates.insert(c, position.try_into().unwrap());
+    // Crates parsing
+    let crates_str = fill_empty_crates(lines.clone(), max_height);
+    for line in crates_str.lines() {
+        let chars = line.chars().filter(|c| *c != '[' && *c != ']');
+        for (i, c) in chars.enumerate() {
+            crates
+                .entry((i + 1) as i32)
+                .or_insert_with(Vec::new)
+                .push(c);
         }
     }
 
-    for line in lines[num_crates..]
+    // Moves parsing
+    for line in lines[max_height..]
         .iter()
         .filter(|line| line.starts_with("move"))
     {
@@ -39,8 +53,8 @@ fn parse_input(input: String) -> (HashMap<char, i32>, Vec<(char, i32, i32)>) {
         moves.push((crate_num, from, to));
     }
 
-    // println!("crates: {:?}", crates);
-    // println!("moves: {:?}", moves);
+    println!("crates: {:?}", crates);
+    println!("moves: {:?}", moves);
     // println!("input: {}", input);
     return (crates, moves);
 }
@@ -54,15 +68,13 @@ pub fn solution(_input: String) {
             let mut input = String::new();
             let _ = buff_reader.read_to_string(&mut input);
             println!("{}", input);
-            
-            // let (crates, moves) = parse_input(input);
+
+            let (crates, moves) = parse_input(input);
             // println!("{:?}", crates);
             // println!("{:?}", moves);
-        },
+        }
         Err(error) => {
             println!("Error: {}", error);
         }
     }
-
-    // parse_input();
 }
