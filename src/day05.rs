5f0b3de53812cb045036424888eb6a93df9ff763
cisdeli <pedrohcisdeli@gmail.com>
2024-03-14 00:10:37 -0500

    Finished day 05 part 1 For some reason getting the input directly from the URL didn't work. Fix this later

--- src/day05.rs
+++ src/day05.rs
@@ -19,9 +19,9 @@ fn fill_empty_crates(lines: Vec<&str>, max_height: usize) -> String {
     crates_str
 }
 
-fn parse_input(input: String) -> (HashMap<i32, Vec<char>>, Vec<(char, i32, i32)>) {
+fn parse_input(input: String) -> (HashMap<i32, Vec<char>>, Vec<(i32, i32, i32)>) {
     let mut crates: HashMap<i32, Vec<char>> = HashMap::new();
-    let mut moves: Vec<(char, i32, i32)> = Vec::new();
+    let mut moves: Vec<(i32, i32, i32)> = Vec::new();
     let lines: Vec<&str> = input.lines().collect();
 
     let max_height = lines
@@ -34,10 +34,13 @@ fn parse_input(input: String) -> (HashMap<i32, Vec<char>>, Vec<(char, i32, i32)>
     for line in crates_str.lines() {
         let chars = line.chars().filter(|c| *c != '[' && *c != ']');
         for (i, c) in chars.enumerate() {
-            crates
-                .entry((i + 1) as i32)
-                .or_insert_with(Vec::new)
-                .push(c);
+            if c.is_alphabetic() {
+                // not pushing empty crates
+                crates
+                    .entry((i + 1) as i32)
+                    .or_insert_with(Vec::new)
+                    .push(c);
+            }
         }
     }
 
@@ -47,7 +50,7 @@ fn parse_input(input: String) -> (HashMap<i32, Vec<char>>, Vec<(char, i32, i32)>
         .filter(|line| line.starts_with("move"))
     {
         let parts: Vec<&str> = line.split_whitespace().collect();
-        let crate_num = parts[1].chars().next().unwrap();
+        let crate_num = parts[1].parse::<i32>().unwrap();
         let from = parts[3].parse::<i32>().unwrap();
         let to = parts[5].parse::<i32>().unwrap();
         moves.push((crate_num, from, to));
@@ -55,26 +58,60 @@ fn parse_input(input: String) -> (HashMap<i32, Vec<char>>, Vec<(char, i32, i32)>
 
     println!("crates: {:?}", crates);
     println!("moves: {:?}", moves);
-    // println!("input: {}", input);
-    return (crates, moves);
+    (crates, moves)
+}
+
+fn solve(mut crates: HashMap<i32, Vec<char>>, moves: Vec<(i32, i32, i32)>) {
+    for (_, vec) in crates.iter_mut() {
+        vec.reverse();
+    }
+    for mv in moves.iter() {
+        let crate_qtt = mv.0;
+        let from = mv.1;
+        let to = mv.2;
+
+        let from_stack = crates.get_mut(&from).unwrap();
+
+        let mut crates_aux = Vec::new();
+        for _ in 0..crate_qtt {
+            if let Some(c) = from_stack.pop() {
+                crates_aux.push(c);
+            }
+        }
+        crates.get_mut(&to).unwrap().extend(crates_aux);
+        println!("moved");
+        println!("{:?}", crates);
+    }
+
+    let mut sorted_crates: Vec<_> = crates.into_iter().collect();
+    sorted_crates.sort_by_key(|&(key, _)| key);
+    for (key, vec) in sorted_crates {
+        if let Some(last_char) = vec.last() {
+            println!("Key {}: {}", key, last_char);
+        } else {
+            println!("Key {}: Empty vector", key);
+        }
+    }
 }
 
 pub fn solution(_input: String) {
     let file = File::options().read(true).open("src/input.txt");
-
+    
     match file {
         Ok(file) => {
             let mut buff_reader = BufReader::new(file);
             let mut input = String::new();
             let _ = buff_reader.read_to_string(&mut input);
             println!("{}", input);
-
+    
             let (crates, moves) = parse_input(input);
-            // println!("{:?}", crates);
-            // println!("{:?}", moves);
+            solve(crates, moves);
         }
         Err(error) => {
             println!("Error: {}", error);
         }
     }
+
+    // let (crates, moves) = parse_input(input);
+    // solve(crates, moves);
 }
